da7710e5f61cfe2a860b9ae3b0035485b23b1876
FunkyMood <nicholasferrari95@gmail.com>
2026-06-10 12:47:47 +0200

    Added scan day

--- utilities.js
+++ utilities.js
@@ -1,15 +1,18 @@
 export const Utility = {
     getSyncTimeLabel(date) {
-        const hour = date.getHours()
+        const hour = date.getHours();
+        const today = new Date();
+        const isToday = date.toDateString() === today.toDateString();
+        const dayLabel = isToday ? '' : ` - ${date.getDate()} ${date.toLocaleString('it-IT', { month: 'short' })}`
         switch (true) {
             case hour >= 0 && hour < 6:
-                return '🌙 "Se leggi questo forse è meglio che tu vada a letto sai?"'
+                return `🌙 "Se leggi questo forse è meglio che tu vada a letto sai?"${dayLabel}`
             case hour >= 6 && hour < 12:
-                return '🌅 Scan di mattina'
+                return `🌅 Scan di mattina${dayLabel}`
             case hour >= 12 && hour < 18:
-                return '☀️ Scan di pomeriggio'
+                return `☀️ Scan di pomeriggio${dayLabel}`
             default:
-                return '🌆 Scan di sera'
+                return `🌆 Scan di sera${dayLabel}`
         }
     },
     copperToGold(copper) {
