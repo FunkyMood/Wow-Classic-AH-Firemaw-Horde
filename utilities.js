a3237fc69b40f383ff65e194d2c09beacf387a83
FunkyMood <nicholasferrari95@gmail.com>
2026-06-10 13:49:34 +0200

    Adjust label

--- utilities.js
+++ utilities.js
@@ -6,13 +6,13 @@ export const Utility = {
         const dayLabel = isToday ? '' : ` - ${date.getDate()} ${date.toLocaleString('it-IT', { month: 'short' })}`
         switch (true) {
             case hour >= 0 && hour < 6:
-                return `🌙 "If you read this you have to go to bed and you know it" - ${dayLabel}`
+                return `🌙 "If you read this you have to go to bed and you know it" ${dayLabel}`
             case hour >= 6 && hour < 12:
-                return `🌅 Morning scan - ${dayLabel}`
+                return `🌅 Morning scan ${dayLabel}`
             case hour >= 12 && hour < 18:
-                return `☀️ Afternoon scan - ${dayLabel}`
+                return `☀️ Afternoon scan ${dayLabel}`
             default:
-                return `🌆 Evning scan - ${dayLabel}`
+                return `🌆 Evning scan ${dayLabel}`
         }
     },
     copperToGold(copper) {
